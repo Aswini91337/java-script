deb8e9b4f9adcfbfc4fe8b628b301f9f0aa92bfa
Aswini91337 <aswinisweety2556@gmail.com>
2025-02-21 08:34:14 -0800

    Add files via upload

---
+++ 1_basics.js
@@ -0,0 +1,22 @@
+const accountId = 144553
+let accountEmail = "Aswinisweety2556@gmail.com"
+var accountPassword = "12345"
+accountCity = "kurnool"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "aswinisweety2556@gmail.com.com"
+accountPassword ="25561813"
+accountCity = "kurnool"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
