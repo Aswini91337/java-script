f1a7cc35fac64ebf307d44db4def1be0ebcfdc90
Aswini91337 <aswinisweety2556@gmail.com>
2025-02-21 08:31:33 -0800

    Add files via upload

---
+++ string.js
@@ -0,0 +1,12 @@
+let name ="GpCeT"
+let splitName = name.substring(0,3)  
+let splitName1 = name.slice(0,3)  
+
+const newStringOne = "                               GPCET    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+ string1 = "aswini"
+ string2 ="krish"
+ sumOfString1String2 = string1 +" "+ string2
+console.log(sumOfString1String2)
